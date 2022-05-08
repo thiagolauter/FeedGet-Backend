34fb8af4f256e7080736b46c52ad9ef592f1b255
Thiago Lauter <thiagolauterti@gmail.com>
2022-05-07 23:46:29 -0300

    Fixing cors error

--- src/routes.ts
+++ src/routes.ts
@@ -9,16 +9,22 @@ export const routes = express.Router()
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body
 
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-    )
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
 
-    await submitFeedbackUseCase.execute({
-        type, comment, screenshot
-    })
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        )
 
-    return res.status(201).send()
+        await submitFeedbackUseCase.execute({
+            type, comment, screenshot
+        })
+
+        return res.status(201).send()
+    } catch (err) {
+        return res.status(500).send();
+    }
+
 })
